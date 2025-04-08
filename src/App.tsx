b72c288d70860220e09a0b78bb4799e22391ee84
lanovich <kolaemelanov60@gmail.com>
2025-04-08 15:57:30 +0300

    Реализовали основной компонент блога с роутингом

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
-function App() {
+const Header = () => <div>Шапка</div>;
+const Footer = () => <div>Шапка</div>;
 
+export const Blog = () => {
   return (
-    <>
-      <div>123</div>
-      <i className="fa fa-camera-retro"></i>
-    </>
-  )
-}
-
-export default App
+    <div className="container">
+      <Header />
+      <div className="content">
+        <h2 className="heading2">Контент страницы</h2>
+        <Routes>
+          <Route path="/" element={<div>Главная страница</div>}/>
+          <Route path="/login" element={<div>Авторизация</div>}/>
+          <Route path="/register" element={<div>Регистрация</div>}/>
+          <Route path="/users" element={<div>Пользователи</div>}/>
+          <Route path="/post" element={<div>Новая статья</div>}/>
+          <Route path="/post/:postId" element={<div>Статья</div>}/>
+          <Route path="*" element={<div>Ошибка</div>}/>
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  );
+};
